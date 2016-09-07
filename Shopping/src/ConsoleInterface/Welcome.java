cf5066b078dea2e5070e5e610b7f260cb75d49ef
Dhavaleswar <93dhavaleshwar@gmail.com>
2016-09-07 07:21:22 +0530

    Included methods to display brand names, model from selected brand model and total description of given brand and mobile

--- Shopping/src/ConsoleInterface/Welcome.java
+++ Shopping/src/ConsoleInterface/Welcome.java
@@ -1,7 +1,35 @@
 package ConsoleInterface;
 
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import ShoppingDatabase.DatabaseOperations;
+
 public class Welcome {
+			
 	public static void main(String[] args) {
-		System.out.println("Welcome MSCI Shopping Portal");
+		System.out.println("Welcome MSCI Mobile Shopping Portal");
+		DatabaseOperations DB_Ops = new DatabaseOperations();
+		System.out.println("Available Brands:");
+		DB_Ops.getMobileBrands();
+		
+		String brand;
+		System.out.println("Enter brand name:");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+	        String str = reader.readLine();
+	        brand="\'"+ str +"\' ";
+	        DB_Ops.getMobileModels(brand);	
+	        String model;
+			System.out.println("Enter model name:");
+			String str1 = reader.readLine();
+	        model="\'"+ str1+"\' ";
+	        DB_Ops.getModelDetails(brand, model);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }		
+	
 	}
 }
--- Shopping/src/ShoppingDatabase/DatabaseOperations.java
+++ Shopping/src/ShoppingDatabase/DatabaseOperations.java
@@ -1,6 +1,74 @@
 package ShoppingDatabase;
 
-public class DatabaseOperations {
-	
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.*;
 
+public class DatabaseOperations {
+	private String DB_URL = "jdbc:mysql://localhost:3306/INVENTORY";
+	private String USER = "root";
+	private String PASS = "itsmyroot";
+	//private Connection conn;
+	
+	public void getMobileBrands() {
+		try {
+			//System.out.println(USER);
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			//System.out.println("Connection: "+conn);		
+			java.sql.Statement stmDha = conn.createStatement();
+			String query = "SELECT BRAND FROM SMARTPHONES1 GROUP BY BRAND";
+			ResultSet rs =  stmDha.executeQuery(query);
+			while (rs.next()) {
+				System.out.println(rs.getString("BRAND")+" ");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void getMobileModels(String brand) {
+		try {
+			//System.out.println(USER);
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			//System.out.println("Connection: "+conn);		
+			java.sql.Statement stmDha = conn.createStatement();
+			String query = "SELECT PRODUCT_NAME FROM SMARTPHONES1 WHERE BRAND =" + brand+"GROUP BY PRODUCT_NAME";
+			ResultSet rs =  stmDha.executeQuery(query);
+			while (rs.next()) {
+				System.out.println(rs.getString("PRODUCT_NAME")+" ");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void getModelDetails(String brand, String model) {
+		try {
+			//System.out.println(USER);
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			//System.out.println("Connection: "+conn);		
+			java.sql.Statement stmDha = conn.createStatement();
+			String query = "SELECT * FROM SMARTPHONES1 WHERE BRAND =" + brand+"AND PRODUCT_NAME="+model;
+			ResultSet rs =  stmDha.executeQuery(query);
+			while (rs.next()) {
+				System.out.print(rs.getString("BRAND")+" ");
+				System.out.print(rs.getString("PRODUCT_NAME")+" ");
+				System.out.print(rs.getInt("PRICE")+" ");
+				System.out.print(rs.getString("COLOUR")+" ");
+				System.out.println(rs.getBoolean("EMI")+" ");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
