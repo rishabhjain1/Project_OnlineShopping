90f621fb07b276a4dfec2c40f9ef008c8027d4f6
Dhavaleswar <93dhavaleshwar@gmail.com>
2016-09-23 01:35:44 +0530

    basic template for adding category is finished

---
+++ Shopping/src/Interface/CategoryServlet.java
@@ -0,0 +1,49 @@
+package Interface;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ShoppingDatabase.DBSession;
+
+@WebServlet("/CategoryServlet")
+public class CategoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public CategoryServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out =response.getWriter();
+		int pid = Integer.parseInt(request.getParameter("pid"));
+		String category = request.getParameter("category");
+		System.out.println(category);
+		request.getRequestDispatcher("/updatingCatServlet?pid="+pid).include(request,response);
+		try {
+			DBSession dbConnection = new DBSession();
+			String query = "SELECT PRODUCT_NAME FROM PRODUCTS WHERE PID="+pid;
+			ResultSet rs=dbConnection.runQuery(query);
+			rs.next();
+			String name = rs.getString("PRODUCT_NAME");
+			String query1 = "INSERT INTO PRODUCT_CATEGORY (PID,CATEGORY) VALUES ("+pid+", '"+category+"')";
+			dbConnection.runQuery(query1);
+			out.println("<script type=\"text/javascript\">");
+	        out.print("alert('Category linked to "+name+"');");
+	        out.println("</script>");
+		}catch (SQLException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
