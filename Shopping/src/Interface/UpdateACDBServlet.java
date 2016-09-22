f913311a6cc045ea2c254e111ed5c1825ba99969
Dhavaleswar <93dhavaleshwar@gmail.com>
2016-09-23 00:53:40 +0530

    basic template for adding new product and adding attributes

--- Shopping/src/Interface/UpdateACDBServlet.java
+++ Shopping/src/Interface/UpdateACDBServlet.java
@@ -14,11 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 import ShoppingDatabase.DBSession;
 
 
-@WebServlet("/UpdateAC_DBServlet")
-public class UpdateAC_DBServlet extends HttpServlet {
+@WebServlet("/UpdateACDBServlet")
+public class UpdateACDBServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public UpdateAC_DBServlet() {
+    public UpdateACDBServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,8 +31,6 @@ public class UpdateAC_DBServlet extends HttpServlet {
 			DBSession dbConnection = new DBSession();
 			String query = "SELECT DISTINCT PRODUCTS.PID,QUANTITY, PRODUCT_NAME,PRICE FROM PRODUCTS JOIN PRODUCT_CATEGORY ON PRODUCTS.PID=PRODUCT_CATEGORY.PID WHERE PRODUCT_NAME LIKE '%"+search+"%'OR CATEGORY LIKE '%"+search+"%'";
 			ResultSet rs = dbConnection.runQuery(query);
-			//out.print("hi");
-			//out.print("<html><head><meta charset=ISO-8859-1><title>Insert title here</title></head><body>");
 			out.print("<table class='upd-table'><tr><th>Product Name</th><th>Price</th><th>Quantity</th><th colspan = '4'>Action</th></tr>");
 			
 			while(rs.next()) {
@@ -44,8 +42,8 @@ public class UpdateAC_DBServlet extends HttpServlet {
 							+ "<td><a href = /Shopping/SpecificationServlet?p="+pid+">"+productName+"</a></td>"
 							+ "<td id ='price'>"+price+"</td>"
 							+ "<td id = 'quant'>"+ quantity+"</td>"
-							+ "<td id = 'btn'><a href = /Shopping/updatingDBServlet?pid="+pid+"><button class='w3-btn w3-blue'> UPDATE</button></a></td>"							
-							+ "<td id = 'btn'><a href = /Shopping/deletingDBServlet?pid="+pid+"><button class='w3-btn w3-red'> DELETE</button></a></td>"
+							+ "<td id = 'btn'><a href = /Shopping/updatingAtrrServlet?pid="+pid+"><button class='w3-btn w3-blue'>ATTRIBUTE</button></a></td>"							
+							+ "<td id = 'btn'><a href = /Shopping/updatingCatServlet?pid="+pid+"><button class='w3-btn w3-red'> CATEGORY</button></a></td>"
 						+"</tr>");
 				}
 				dbConnection.close();
